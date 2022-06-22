9f1f77054dee106d5ac2d46cc5f01fe09e59ef64
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-22 23:59:25 +0700

    update route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Customer from "./pages/Customer";
+import Company from "./pages/Company";
 
 const App = () => {
   return (
     <Routes>
-      <Route index element={<Login />} />
       <Route path="login" element={<Login />} />
       <Route element={<ProtectedRoute />}>
-        <Route path="home" element={<Home />} />
+        <Route path="/" element={<Home />}>
+          <Route path="customer" element={<Customer />} />
+          <Route path="company" element={<Company />} />
+        </Route>
       </Route>
     </Routes>
   );
