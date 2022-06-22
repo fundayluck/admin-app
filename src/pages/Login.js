80b85ccbd6107f04d895d60aa8d3c8923ce73120
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-22 23:58:32 +0700

    update Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -37,7 +37,7 @@ const Login = () => {
       setAuth({ user, pass, access_token });
       setUser("");
       setPass("");
-      navigate("/home");
+      navigate("/");
     } catch (err) {
       if (!err.response) {
         setErrMess("Network Error");
@@ -61,28 +61,64 @@ const Login = () => {
         >
           {errMess}
         </p>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="user">User:</label>
-          <input
-            type="text"
-            id="user"
-            ref={userRef}
-            autoComplete="off"
-            onChange={(e) => setUser(e.target.value)}
-            value={user}
-            required
-          />
+        <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+          <div className="max-w-md w-full space-y-8">
+            <div>
+              <img
+                className="mx-auto h-12 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                alt="Workflow"
+              />
+              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                Sign in to your account
+              </h2>
+            </div>
+            <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+              <input type="hidden" name="remember" defaultValue="true" />
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div>
+                  <label htmlFor="email-address" className="sr-only">
+                    Email address
+                  </label>
+                  <input
+                    type="text"
+                    id="user"
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(e) => setUser(e.target.value)}
+                    value={user}
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Email address"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password" className="sr-only">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    id="pass"
+                    onChange={(e) => setPass(e.target.value)}
+                    value={pass}
+                    required
+                    className="appearance-none mb-3 rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Password"
+                  />
+                </div>
 
-          <label htmlFor="pass">Password:</label>
-          <input
-            type="password"
-            id="pass"
-            onChange={(e) => setPass(e.target.value)}
-            value={pass}
-            required
-          />
-          <button>Sign In</button>
-        </form>
+                <div>
+                  <button
+                    type="submit"
+                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Sign in
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </section>
     </>
   );
