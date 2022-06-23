e5f586a80b5d64cb5265819179d09e0c758b0f28
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-23 08:54:45 +0700

    fix styling navlink

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "../assets/logo.png";
 import { TbBuildingSkyscraper as CompanyProfile } from "react-icons/tb";
 import { RiHome8Line as Home } from "react-icons/ri";
-import { MdAddBox as AddCustomer } from "react-icons/md";
+import { TbUsers as AddCustomer } from "react-icons/tb";
 import { NavLink } from "react-router-dom";
 
 const dataSidebar1 = [
@@ -23,17 +23,24 @@ const Sidebar = ({ children }) => {
         <img src={logo} alt="logo" className="pb-[10px]" />
         {dataSidebar1.map((item, index) => (
           <NavLink
-            className="flex w-[60px] h-[40px] justify-center items-center hover:bg-[#EBF2EC]"
+            className={({ isActive }) =>
+              isActive
+                ? "flex w-[60px] h-[40px] justify-center items-center bg-[#EBF2EC] border-l-[5px] border-l-[#8FB493]"
+                : "flex w-[60px] h-[40px] justify-center items-center hover:bg-[#EBF2EC]"
+            }
             key={index}
             to={item.path}
-            activeClassName="bg-[#EBF2EC] border-l-[5px] border-l-[#8FB493]"
           >
             {item.logo}
           </NavLink>
         ))}
         <div className="flex w-[60px] h-[20px] justify-center items-center"></div>
         <NavLink
-          className="flex w-[60px] h-[40px] justify-center items-center hover:bg-[#EBF2EC]"
+          className={({ isActive }) =>
+            isActive
+              ? "flex w-[60px] h-[40px] justify-center items-center bg-[#EBF2EC] border-l-[5px] border-l-[#8FB493] "
+              : "flex w-[60px] h-[40px] justify-center items-center hover:bg-[#EBF2EC]"
+          }
           to="/customer"
         >
           <AddCustomer color="#B2B2B2" />
