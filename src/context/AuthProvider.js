c0e64322f74876aaa0cee0907f2fd759f1311f6b
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-23 08:27:02 +0700

    save localdata

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,10 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState({});
-
+  const [auth, setAuth] = useState(() => {
+    const localData = localStorage.getItem("auth");
+    console.log(localData);
+    return localData ? JSON.parse(localData) : {};
+  });
+  useEffect(() => {
+    localStorage.setItem("auth", JSON.stringify(auth));
+  }, [auth]);
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
       {children}
