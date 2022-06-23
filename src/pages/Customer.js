941a379576d75598186ad9f21d72a555aaf317d9
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-23 12:32:21 +0700

    trying to get data

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useAuth from "../ahooks/useAuth";
+import Api from "../api/Api";
+
+const CUSTOMERS_URL = "/customers";
 
 const Customer = () => {
-  return <div>Customer</div>;
+  const { auth } = useAuth();
+  const [loading, setLoading] = useState(false);
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        setLoading(true);
+        const response = await Api.get(CUSTOMERS_URL, {
+          headers: {
+            Authorization: auth.access_token,
+          },
+        });
+        setMessage(response.data.message);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
+    };
+    fetch();
+  }, [auth.access_token]);
+
+  return <div>{loading ? "Loading..." : message}</div>;
 };
 
 export default Customer;
