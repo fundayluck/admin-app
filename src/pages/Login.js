dd61cd02454b29a12b50359b1df7327c3d8d5708
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-23 08:26:17 +0700

    change logo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import { useRef, useState, useEffect } from "react";
 import useAuth from "../ahooks/useAuth";
 import { useNavigate } from "react-router-dom";
 import Api from "../api/Api";
+import logo from "../assets/logo.png";
 const LOGIN_URL = "/auth/login";
 
 const Login = () => {
@@ -31,7 +32,6 @@ const Login = () => {
         email: "akun12@mig.id",
         password: "BA8A9E03",
       });
-
       console.log(JSON.stringify(response?.data));
       const access_token = response?.data?.access_token;
       setAuth({ user, pass, access_token });
@@ -61,14 +61,10 @@ const Login = () => {
         >
           {errMess}
         </p>
-        <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="bg-[#ffffff] h-[100%] flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
           <div className="max-w-md w-full space-y-8">
             <div>
-              <img
-                className="mx-auto h-12 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                alt="Workflow"
-              />
+              <img className="mx-auto h-12 w-auto" src={logo} alt="Workflow" />
               <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                 Sign in to your account
               </h2>
